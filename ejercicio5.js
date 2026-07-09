e0ce5fc37605ee64bd9614898b7b42a52c8d7356
JulianCastellanos <itzjulian14@gmail.com>
2026-07-09 16:15:38 -0500

    subiendo ejercicio 5

--- ejercicio5.js
+++ ejercicio5.js
@@ -1,8 +1,34 @@
 const prompt = require("prompt-sync")();
- function Vehiculor(marca, modelo, velocidad, ruedas, precio) {
+function Vehiculo(marca, modelo, ruedas, precio) {
     this.marca = marca;
     this.modelo = modelo
     this.ruedas = ruedas;
     this.precio = precio;
+    this.velocidad = 0;
+
+    this.acelerar = function () {
+        this.velocidad += 10;
+        console.log(`Vehiculo  ${marca} ${modelo} acelerando a ${this.velocidad}km/h`)
+
+    }
+    this.frenar = function () {
+        this.velocidad = 0;
+        console.log(`Vehiculo ${marca} ${modelo} Frenando, Velocidad: ${this.velocidad}km/h`)
+
+    }
+    this.pitar = function () {
+        console.log(`Vehiculo ${marca} ${modelo} peeep peeep!`)
+    }
 }
-let 
+let marca = prompt("Ingresa la marca de tu vehiculo: ")
+let modelo = prompt("Ingresa el modelo de tu vehiculo: ")
+let ruedas = prompt("Ingresa cuantas ruedas tiene tu vehiculo: ")
+let precio = prompt("Ingresa el precio de tu vehiculo: ")
+
+let vehiculo = new Vehiculo(marca, modelo, ruedas, precio)
+
+vehiculo.acelerar();
+vehiculo.acelerar();
+vehiculo.frenar();
+vehiculo.pitar();
+
